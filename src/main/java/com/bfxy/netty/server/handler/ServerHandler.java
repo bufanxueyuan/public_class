b19e2ce7a3bc2a3e3d654f7d59b8740490e5abb5
baihezhuo <baihezhuo@meituan.com>
2018-05-24 17:07:03 +0800

    netty - 公开课

---
+++ src/main/java/com/bfxy/netty/server/handler/ServerHandler.java
@@ -0,0 +1,56 @@
+package com.bfxy.netty.server.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+//ChannelHandlerAdapter
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("server channel active... ");
+	}
+
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+			ByteBuf buf = (ByteBuf) msg;
+			byte[] req = new byte[buf.readableBytes()];
+			buf.readBytes(req);
+			String body = new String(req, "utf-8");
+			System.out.println("Server :" + body );
+			String response = "返回给客户端的响应信息, " + body ;
+			
+//			PooledByteBufAllocator pool = new PooledByteBufAllocator();
+//			PooledByteBufAllocator.DEFAULT.heapBuffer();
+			
+			//1.
+			ctx.writeAndFlush(Unpooled.wrappedBuffer(response.getBytes()));		
+			
+			//2. 改进点
+//			ByteBuf directBuf = Unpooled.directBuffer(1024*10, 1024*32);
+//			directBuf.writeBytes(response.getBytes());
+//			ctx.writeAndFlush(directBuf);	
+
+			//.addListener(ChannelFutureListener.CLOSE);
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx)
+			throws Exception {
+		System.out.println("读完了");
+		ctx.flush();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable t)
+			throws Exception {
+		System.err.println(t);
+		ctx.close();
+	}
+
+}
